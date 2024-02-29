13617f9d82733f6cf713ab2eea4d6d6cc8d87e89
Luiz Gustavo <gusynho94@gmail.com>
2024-02-29 17:20:49 -0300

    alguns bugs

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,29 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import PostComments from './index'; // Importe o componente correto
+import '@testing-library/jest-dom/extend-expect';
 
-describe('Teste para o componente PostComment', () => {
+
+describe('Teste para o componente PostComments', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComments />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it('Deve adicionar dois comentários corretamente', () => {
+        render(<PostComments />);
+
+        const commentInput = screen.getByTestId('comment-input');
+        const commentSubmitButton = screen.getByTestId('comment-submit');
+
+        fireEvent.change(commentInput, { target: { value: 'Primeiro comentário' } });
+        fireEvent.click(commentSubmitButton);
+        expect(screen.getByTestId('comment-0')).toBeInTheDocument();
+        expect(screen.getByTestId('comment-0')).toHaveTextContent('Primeiro comentário');
+
+        fireEvent.change(commentInput, { target: { value: 'Segundo comentário' } });
+        fireEvent.click(commentSubmitButton);
+
+        expect(screen.getByTestId('comment-1')).toBeInTheDocument();
+        expect(screen.getByTestId('comment-1')).toHaveTextContent('Segundo comentário');
     });
 });
